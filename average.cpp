f31e8ca610a1bec29a7cd02877b9d523536fb3fc
Beam <b3bb0z@gmail.com>
2025-02-21 16:58:19 +0700

    FN

--- average.cpp
+++ average.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cout << "Please input numbers to find average." << std::endl;
+        return 0;
+    }
+
+    std::vector<double> numbers;
+    for (int i = 1; i < argc; ++i) {
+        std::istringstream iss(argv[i]);
+        double num;
+        if (iss >> num) {
+            numbers.push_back(num);
+        }
+    }
+
+    double sum = 0;
+    for (const auto& num : numbers) {
+        sum += num;
+    }
+
+    double average = sum / numbers.size();
+
+    std::cout << "---------------------------------" << std::endl;
+    std::cout << "Average of " << numbers.size() << " numbers = " << std::fixed << std::setprecision(0) <<average << std::endl;
+    std::cout << "---------------------------------" << std::endl;
+
+    return 0;
+}
 
--- calculator.cpp
+++ calculator.cpp
@@ -1,66 +1,91 @@
 #include <windows.h>
+#include <string>
 
-/* This is where all the input to the window goes to */
-LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
-	switch(Message) {
-		
-		/* Upon destruction, tell the main thread to stop */
-		case WM_DESTROY: {
-			PostQuitMessage(0);
-			break;
-		}
-		
-		/* All other messages (a lot of them) are processed using default procedures */
-		default:
-			return DefWindowProc(hwnd, Message, wParam, lParam);
-	}
-	return 0;
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+void PerformCalculation(HWND hwnd, char operation);
+
+HWND hEdit1, hEdit2;
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    WNDCLASS wc = { 0 };
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = "Calculator";
+    wc.hbrBackground = CreateSolidBrush(RGB(255, 0, 0)); // Red background
+    
+    RegisterClass(&wc);
+    HWND hwnd = CreateWindow("Calculator", "My Calculator", WS_OVERLAPPED | WS_SYSMENU,
+        CW_USEDEFAULT, CW_USEDEFAULT, 250, 200, NULL, NULL, hInstance, NULL);
+    
+    ShowWindow(hwnd, nCmdShow);
+    
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return 0;
 }
 
-/* The 'main' function of Win32 GUI programs: this is where execution starts */
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-	WNDCLASSEX wc; /* A properties struct of our window */
-	HWND hwnd; /* A 'HANDLE', hence the H, or a pointer to our window */
-	MSG msg; /* A temporary location for all messages */
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+    case WM_CREATE:
+        CreateWindow("STATIC", "Please input two numbers", WS_VISIBLE | WS_CHILD,
+            20, 10, 200, 20, hwnd, NULL, NULL, NULL);
+        
+        hEdit1 = CreateWindow("EDIT", "", WS_VISIBLE | WS_CHILD | WS_BORDER | ES_AUTOHSCROLL,
+            20, 40, 200, 20, hwnd, NULL, NULL, NULL);
+        hEdit2 = CreateWindow("EDIT", "", WS_VISIBLE | WS_CHILD | WS_BORDER | ES_AUTOHSCROLL,
+            20, 70, 200, 20, hwnd, NULL, NULL, NULL);
+        
+        CreateWindow("BUTTON", "+", WS_VISIBLE | WS_CHILD,
+            20, 100, 40, 30, hwnd, (HMENU)1, NULL, NULL);
+        CreateWindow("BUTTON", "-", WS_VISIBLE | WS_CHILD,
+            70, 100, 40, 30, hwnd, (HMENU)2, NULL, NULL);
+        CreateWindow("BUTTON", "*", WS_VISIBLE | WS_CHILD,
+            120, 100, 40, 30, hwnd, (HMENU)3, NULL, NULL);
+        CreateWindow("BUTTON", "/", WS_VISIBLE | WS_CHILD,
+            170, 100, 40, 30, hwnd, (HMENU)4, NULL, NULL);
+        break;
+    
+    case WM_COMMAND:
+        if (LOWORD(wParam) >= 1 && LOWORD(wParam) <= 4) {
+            PerformCalculation(hwnd, (LOWORD(wParam) == 1 ? '+' :
+                                      LOWORD(wParam) == 2 ? '-' :
+                                      LOWORD(wParam) == 3 ? '*' : '/'));
+        }
+        break;
+        
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+    return 0;
+}
 
-	/* zero out the struct and set the stuff we want to modify */
-	memset(&wc,0,sizeof(wc));
-	wc.cbSize	 = sizeof(WNDCLASSEX);
-	wc.lpfnWndProc	 = WndProc; /* This is where we will send messages to */
-	wc.hInstance	 = hInstance;
-	wc.hCursor	 = LoadCursor(NULL, IDC_ARROW);
-	
-	/* White, COLOR_WINDOW is just a #define for a system color, try Ctrl+Clicking it */
-	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
-	wc.lpszClassName = "WindowClass";
-	wc.hIcon	 = LoadIcon(NULL, IDI_APPLICATION); /* Load a standard icon */
-	wc.hIconSm	 = LoadIcon(NULL, IDI_APPLICATION); /* use the name "A" to use the project icon */
-
-	if(!RegisterClassEx(&wc)) {
-		MessageBox(NULL, "Window Registration Failed!","Error!",MB_ICONEXCLAMATION|MB_OK);
-		return 0;
-	}
-
-	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","Caption",WS_VISIBLE|WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, /* x */
-		CW_USEDEFAULT, /* y */
-		640, /* width */
-		480, /* height */
-		NULL,NULL,hInstance,NULL);
-
-	if(hwnd == NULL) {
-		MessageBox(NULL, "Window Creation Failed!","Error!",MB_ICONEXCLAMATION|MB_OK);
-		return 0;
-	}
-
-	/*
-		This is the heart of our program where all input is processed and 
-		sent to WndProc. Note that GetMessage blocks code flow until it receives something, so
-		this loop will not produce unreasonably high CPU usage
-	*/
-	while(GetMessage(&msg, NULL, 0, 0) > 0) { /* If no error is received... */
-		TranslateMessage(&msg); /* Translate key codes to chars if present */
-		DispatchMessage(&msg); /* Send it to WndProc */
-	}
-	return msg.wParam;
+void PerformCalculation(HWND hwnd, char operation) {
+    char buffer1[256], buffer2[256];
+    GetWindowText(hEdit1, buffer1, 256);
+    GetWindowText(hEdit2, buffer2, 256);
+    
+    double num1 = atof(buffer1);
+    double num2 = atof(buffer2);
+    double result = 0;
+    
+    if (operation == '/' && num2 == 0) {
+        MessageBox(hwnd, "Cannot divide by zero!", "Error", MB_OK | MB_ICONERROR);
+        return;
+    }
+    
+    switch (operation) {
+        case '+': result = num1 + num2; break;
+        case '-': result = num1 - num2; break;
+        case '*': result = num1 * num2; break;
+        case '/': result = num1 / num2; break;
+    }
+    
+    std::string resStr = "Result: " + std::to_string(result);
+    MessageBox(hwnd, resStr.c_str(), "Result", MB_OK | MB_ICONINFORMATION);
 }
